6130579f900ef3b0d1d7b87b9e56bba6916862ec
xiaoyao-zhang <183991666280@163.com>
2021-12-26 21:55:11 +0800

    二进制文件读写操作

--- cpp_source/file binary_operator.cpp
+++ cpp_source/file binary_operator.cpp
@@ -10,6 +10,7 @@ public:
     int m_Age;              //年龄
 
 };
+//二进制写文件操作
 void test01()
 {
     //1.包含头文件
@@ -22,6 +23,26 @@ void test01()
     ofs.write((const char*)&p, sizeof(Person));
     //5.关闭文件
     ofs.close();
+}
+//二进制读文件操作
+void test02()
+{
+    //1.包含头文件
+    //2.创建流文件
+    ifstream ifs;
+    //3.打开文件  判断文件是否打开成功
+    ifs.open("person.txt", ios :: in | ios :: binary);
+    if(ifs.is_open())
+    {
+        cout<<"文件打开失败"<<endl;
+        return;
+    }
+    //4.读文件
+    Person p;
+    ifs.read((char*)&p, sizeof(Person));
+    cout<<"姓名："<<p.m_Name<<"年龄："<<p.m_Age<<endl;
+    //5.关闭文件
+    ifs.close();
 }
 int mian()
 {
